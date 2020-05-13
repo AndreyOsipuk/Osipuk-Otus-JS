d8c662c35dcf4cf6e13e102e726d42bbf1e0daed
AndreyOsipuk <45951478+AndreyOsipuk@users.noreply.github.com>
2020-05-13 07:39:19 +0500

    build hw6

---
+++ src/AddData.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { data, category, add } from "./store.js";
+
+  let date, text, selected;
+
+  function handleSubmit() {
+    add({
+      date: date,
+      category: selected,
+      value: text
+    });
+    date = "";
+    text = "";
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <label for="date">Введите дату</label>
+  <input id="date" type="date" bind:value={date} />
+
+  <label for="category">Choose a category:</label>
+  <select id="category" bind:value={selected}>
+    {#each $category as item}
+      <option value={item}>{item}</option>
+    {/each}
+  </select>
+  <label for="value">Введите значение</label>
+  <input id="value" type="text" bind:value={text} />
+  <br />
+
+  <button disabled={!date | !text | !selected} type="submit">Submit</button>
+</form>
---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { data } from "./store.js";
+
+  import AddData from "./AddData.svelte";
+  import Table from "./Table.svelte";
+</script>
+
+<main>
+
+  <h1>Hello svelte!</h1>
+  
+  <Table />
+  <AddData />
+</main>
